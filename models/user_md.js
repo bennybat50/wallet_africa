bef758d515a9b2e401446758c9a05adae4433a33
Young Batimehin <benny.bat51@gmail.com>
2023-04-30 16:06:25 +0100

    Models update

---
+++ models/user_md.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+
+const userSchema=new Schema({
+    firstName:{String, required:true},
+    lastName:{String, required:true},
+    email:{String, required:true},
+    phone:{String, required:true},
+    address:{String, required:true},
+    bvn:{String, required:true,},
+    gender:{String, required:true},
+    status:{Boolean},
+    account_id:{String, require:true}
+})
+
+const users=mongoose.model("Users", userSchema)
+module.exports=users
