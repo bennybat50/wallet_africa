535db31139e912756657f31f833bbe5e930b0b3d
Young Batimehin <benny.bat51@gmail.com>
2023-05-06 13:51:41 +0100

    up

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser=require('body-parser')
 const session=require('express-session')
 const handleBars=require('express-handlebars')
 const path=require('path')
+const mongoose=require('mongoose')
 
 //APP CONFIGURATIONS
 
@@ -14,7 +15,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 //For all Static files (Css, Imgs, Js, Fonts etc)
 let {HOST, PORT}=require('./config/configuration')
- let app_route=require('./routes/app-routes')
+let app_route=require('./routes/app-routes')
+let api_route=require('./routes/api-routes')
  
 //VIEW CONFIGURATION
 app.engine('hbs', handleBars.engine({
@@ -22,11 +24,28 @@ app.engine('hbs', handleBars.engine({
     defaultLayout: 'main', extname: '.hbs'
 }))
 
- app.set('views', path.join(__dirname, 'views'))
- app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+ 
+
+//MONGODB CONNECTION
+mongoose.connect('mongodb://localhost:27017/wallet_africa',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+mongoose.connection.on("error", console.error.bind(console,"Connection Error"))
+mongoose.connection.on("connected", function () {
+    console.log("Mongodb Connected");
+})
 
  //SET ROUTES
  app.use('/', app_route)
+ app.use('/api', api_route)
+ 
+
+
+//SET ROUTES
+app.use('/', app_route)
 
 
 
