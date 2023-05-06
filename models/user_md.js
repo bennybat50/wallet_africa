567d8464e98f48e1dcf7be7ec5f66752033e9709
Young Batimehin <benny.bat51@gmail.com>
2023-05-06 14:27:08 +0100

    upload

--- models/user_md.js
+++ models/user_md.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
 const Schema=mongoose.Schema
 
+//hello
 const userSchema=new Schema({
     firstName:{type:String, required:true},
     lastName:{type:String, required:true},
