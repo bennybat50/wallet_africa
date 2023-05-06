50826c287de6731ca3657f4340597299fbad4e1f
empirewebmd <bubmellz1born@gmail.com>
2023-05-06 13:23:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-const express= require('express')
-const app=express()
-const cors=require('cors')
-const bodyParser=require('body-parser')
-const session=require('express-session')
-const handleBars=require('express-handlebars')
-const path=require('path')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const handleBars = require('express-handlebars')
+const path = require('path')
+const mongoose = require('mongoose')
 
 //APP CONFIGURATIONS
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 //For all Static files (Css, Imgs, Js, Fonts etc)
-let {HOST, PORT}=require('./config/configuration')
- let app_route=require('./routes/app-routes')
- 
+let { HOST, PORT } = require('./config/configuration')
+let app_route = require('./routes/app-routes')
+
 //VIEW CONFIGURATION
-app.engine('hbs',handleBars.engine({layoutsDirectory:path.join(__dirname, 'views/layouts'),
- defaultLayout:'main', extname:'.hbs' }))
+app.engine('hbs', handleBars.engine({
+    layoutsDirectory: path.join(__dirname, 'views/layouts'),
+    defaultLayout: 'main', extname: '.hbs'
+}))
 
- app.set('views', path.join(__dirname, 'views'))
- app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+// Mongodb Connection
+mongoose.connect('mongodb://localhost:27017/wallet_africa', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
- //SET ROUTES
- app.use('/', app_route)
+mongoose.connection.on("error", console.error.bind(console, "connection Error"))
+mongoose.connection.on('connected', function () {
+    console.log("MongodB Connected")
+})
 
 
 
-app.listen(PORT, ()=>{
+
+//SET ROUTES
+app.use('/', app_route)
+
+
+
+app.listen(PORT, () => {
     console.log(`Running on ${HOST}${PORT}`);
 })
