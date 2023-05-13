1f9537c524c9cf28916aef4e478f9957acbcb1c7
empirewebmd <bubmellz1born@gmail.com>
2023-05-13 11:32:50 +0100

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,21 +1,63 @@
-const express=require('express')
-const router=express.Router()
-const userModel=require('../models/user_md')
+const express = require('express')
+const router = express.Router()
+const userModel = require('../models/user_md')
 
 //USER MANAGEMENT ROUTES
-router.post('/create-user', (req, res)=>{
+router.post('/create-user', (req, res) => {
 
-    userModel.create(req.body).then((user)=>{
-        res.send(user)
-        
-    }).catch((err)=>{
+    userModel.create(req.body).then((user) => {
+        res.send({ sucess: true, data: user });
+
+    }).catch((err) => {
         console.log(err);
-        res.send(err)
+        res.send({ "error": true, message: err.message });
     })
-    
+
+})
+
+
+router.get('/users', (req, res) => {
+    userModel.find().lean().then((users) => {
+        res.send({ success: true, data: users });
+    })
+})
+
+router.get('/user/:id', (req, res) => {
+    userModel.findById(req.params.id).lean().then((user) => {
+        res.send({ success: true, data: user });
+    })
+})
+
+
+
+router.put('/user/:id', (req, res) => {
+    userModel.findByIdAndUpdate(req.params.id, req.body).then(() => {
+
+        userModel.findById(req.params.id).lean().then((user) => {
+            res.send({ success: true, msg: "user Updates", data: user });
+        })
+
+    })
 })
 
 
 
 
-module.exports=router
+router.delete('/delete-user/:id', (req, res) => {
+    userModel.findByIdAndDelete(req.params.id, req.body).then(() => {
+        res.send({ success: true, msg: "user deleted" });
+
+
+
+
+
+    })
+})
+
+
+
+
+
+
+
+module.exports = router
